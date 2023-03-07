d6915ae1422c3587c52221e5c6be2de81bb2aec5
sangey <sangeylama1000@gmail.com>
2023-03-07 02:58:02 -0800

    For home

--- smartdoorapp/src/App.js
+++ smartdoorapp/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Navbar from '../src/components/Navbar';
+import Home from '../src/components/Home';
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
