1e479e1d4388ecfa38898c7c916117638166d9fd
sangey <sangeylama1000@gmail.com>
2023-03-07 03:24:43 -0800

    for home

--- smartdoorapp/src/App.js
+++ smartdoorapp/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Doorslidersystem from './components/Doorslidersystem';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     <h1>hello i am tsering</h1>
+     <Navbar></Navbar>
+     <Home></Home>
+     <Doorslidersystem></Doorslidersystem>
      
     </div>
   );
