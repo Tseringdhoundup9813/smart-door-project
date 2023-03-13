a18bdf4793a358448fea25450ed7bcfe449acf2c
tenzin <sangeylama1000@gmail.com>
2023-03-13 11:18:36 +0545

    for contact

--- smartdoorapp/src/App.js
+++ smartdoorapp/src/App.js
@@ -1,17 +1,24 @@
 
+//react router 
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 import './App.css';
 import Doorslidersystem from './components/Doorslidersystem';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 function App() {
   return (
-    <div className="App">
-     
-     <Navbar></Navbar>
-     <Home></Home>
-     <Footer></Footer>
-    </div>
+    <BrowserRouter>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer></Footer>
+  </BrowserRouter>
   );
 }
 
