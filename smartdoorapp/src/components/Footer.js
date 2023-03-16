ec82da5cac7e0313f23072feeab7641e9548d3dd
sangey <sangeylama1000@gmail.com>
2023-03-16 17:54:48 +0545

    product-view desk

--- smartdoorapp/src/components/Footer.js
+++ smartdoorapp/src/components/Footer.js
@@ -6,33 +6,50 @@ function Footer(){
         <div>
             <div className="footer">
                 <div className="row-footer row">
-                    <div className="col-md-3 col-12">
-                        <div className="footer-title text-capitalize">About Us</div>
-                        <div className="footer-para">Smart Doors is an emerging door production Company located in Tokha-3, kathmandu. We provide our consumers with products of the highest caliber and with top rated customer service</div>
+                    <div className="col-sm-3 col-6">
+                        <div className="footer-title text-capitalize">the basis</div>
+                        <div className="footer-ul">
+                            <li>How it works</li>
+                            <li>Finding your fits</li>
+                            <li>Buying on SD</li>
+                        </div>
                     </div>
-                    <div className="col-md-3 col-6">
+                    <div className="col-sm-3 col-6">
                         <div className="footer-title">company</div>
                         <div className="footer-ul">
-                            <li><a href="#">product</a></li>
-                            <li><a href="#">about us</a></li>
-                            <li><a href="#">contact</a></li>
+                            <li>Vision</li>
+                            <li>Story</li>
                         </div>
                     </div>
-                    <div className="col-md-3 col-6">
-                        <div className="footer-title">Contact us</div>
+                    <div className="col-sm-3 col-6">
+                        <div className="footer-title">Get help</div>
                         <div className="footer-ul">
-                            <li>Tokha - 3, kathmandu</li>
-                            <li>9779802036938</li>
-                            <li className="footer-none">Working Time: Mon-Sat: 9 AM – 5:30 PM</li>
-                            <li className="footer-none">admin@smartdoors.com.np</li>
+                            <li>FAQs</li>
+                            <li>Contact Us</li>
                         </div>
                     </div>
-                    <div className="col-md-3 col-12">
-                        <div className="footer-title">Lets talk</div>
-                        <div className="footer-email"><input type="email"  placeholder="Enter your email id"requied/></div>
-                        <div className="footer-sub-btn btn ">subscribe</div>
+                    <div className="col-sm-3 col-6">
+                        <div className="footer-title">be social</div>
+                        <div className="footer-social d-flex ">
+                            <div className="footer-fb"><i class="fa-brands fa-facebook"></i></div>
+                            <div className="footer-linkedin"><i class="fa-brands fa-linkedin"></i></div>
+                        </div>
                     </div>
                 </div>
+
+                    <div className="row row-copy">
+                    <div className="col-12 footer-copy my-2 d-flex align-items-center justify-content-center flex-column">
+                        <div className="copyright">
+                            &copy; 2023 Smart Doors. All Rights Reseved.
+                        </div>
+                        <div className="footer-terms d-flex">
+                            <div className="f-terms">terms of Service</div>
+                            <div className="f-privacy">Privacy Policy</div>
+                            <div className="f-acc">Accessibility Statement</div>
+                            <div className="f-cho">Your Privacy Choices</div>
+                        </div>
+                    </div>
+                    </div>
             </div>
         </div>
     )
--- smartdoorapp/src/components/ProductView.js
+++ smartdoorapp/src/components/ProductView.js
@@ -1,27 +1,126 @@
 // css
 import '../style/Product-view.css';
+import {useState} from 'react';
+
+//slick
+import React, { Component } from "react";
+import Slider from "react-slick";
 
 
+//slick  css
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+//product-img
+import productimg from "../image/products/3d/SD110.jpeg";
+import can from '../image/products/canadian/CD101.jpg';
+import can1 from '../image/products/canadian/CD102.jpg';
+import can2 from '../image/products/canadian/CD103.jpg';
+import can3 from '../image/products/canadian/CD104.jpg';
+
+import db from '../image/products/double/DDGK.jpg';
+
+import mem from '../image/products/membrane/SD11.jpeg';
+import mem1 from '../image/products/membrane/SD101.jpg';
+import mem2 from '../image/products/membrane/SD102.jpg';
+import mem3 from '../image/products/membrane/SD103.jpg';
+import mem4 from '../image/products/membrane/SD104.jpg';
+import mem5 from '../image/products/membrane/SD106.jpg';
+import mem6 from '../image/products/membrane/SD115.jpg';
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red",borderRadius:"50%", height:"30px",width:"30px",padding:"5px",fontSize:"20px" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+
+  
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red",borderRadius:"50%", height:"30px",width:"30px",padding:"5px",fontSize:"20px" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
 function Productview(){
+    const [state, setState]=useState(false);
+    const [size, setSize]= useState(false);
+    console.log(state);
+    const plusIcon=()=>{
+        setState( !state);
+        const para= document.querySelector('.pv-detail-para');
+        para.style.display=`${state?"none":"block"}`
+    }
+    const sizeIcon=()=>{
+        setSize( !size);
+        const size= document.querySelector('.pv-detail-size');
+        size.style.display=`${state?"none":"block"}`
+    }
+    const shareIcon=()=>{
+        setState( !state);
+        const share= document.querySelector('.pv-detail-share');
+        share.style.display=`${state?"none":"flex"}`
+    }
+    const likeimg=[
+        {image:productimg, title:"3d ddors", price:"3800"},
+        {image:can, title:"3d ddors", price:"3800"},
+        {image:can1, title:"3d ddors", price:"3800"},
+        {image:can2, title:"3d ddors", price:"3800"},
+        {image:can3, title:"3d ddors", price:"3800"},
+        {image:db, title:"3d ddors", price:"3800"},
+        {image:mem, title:"3d ddors", price:"3800"},
+        {image:mem1, title:"3d ddors", price:"3800"},
+        {image:mem2, title:"3d ddors", price:"3800"},
+        {image:mem3, title:"3d ddors", price:"3800"},
+        {image:mem4, title:"3d ddors", price:"3800"},
+        {image:mem5, title:"3d ddors", price:"3800"},
+        {image:mem6, title:"3d ddors", price:"3800"},
+    ]
+    //slick 
+    var settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    }
+
+    const heart=()=>{
+        console.log('hi')
+    }
     return(
         <div>   
             {/* large screen */}
 
-            <div className="row pv-row">
-                <div className="col-sm-6">
+            <div className="row pv-row justify-content-around">
+                <div className="col-sm-6 col-12">
                     <div className="row">
-                        <div className="col-3 pv-container">
+                        <div className="col-4 pv-container">
                             <div className="pv-img"></div>
                             <div className="pv-img"></div>
                             <div className="pv-img"></div>
                         </div>
-                        <div className="col-9 pv-main-img">
+                        <div className="col-8 pv-main-img">
                             <div className="pvimg">
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-sm-6 pv-img-detail">
+                <div className="col-sm-4 col-12 pv-img-detail">
                     <div className="pv-heart-icon text-end">
                     <i class="fa-regular fa-heart"></i>
                     </div>
@@ -62,7 +161,9 @@ function Productview(){
                         
                     </div>
 
-                    <div className="pv-more-ad">
+
+                    <div className="pv-membrance">
+                    <div className="pv-more-ad ">
                         <div className="d-flex align-items-center  justify-content-center">
                         <div className="pv-more-checkbox">
                             <div className="pv-more-dark"></div>
@@ -97,11 +198,205 @@ function Productview(){
                           </div>
                         </div>
                         </div>
+
+                        
+                    </div>
+                    <div className="pv-more-detail">
+                            <div className="pv-more-code"> Code : SD123 RW</div>
+                            <div className="ul pv-ul">
+                                <li className="pv-list">Get your own customization</li>
+                                <li className="pv-list">Free shipping</li>
+                                <li className="pv-list">Door to door delivery</li>
+                                <li className="pv-list">Pause or cancel anytime</li>
+                            </div>
+
+                            <div className="pv-more-explore">
+                                <span>Explore Membership</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="pv-more-ad ">
+                        <div className="d-flex align-items-center  justify-content-center">
+                        <div className="pv-more-checkbox">
+                            <div className="pv-more-dark"></div>
+                        </div>
+                        </div>   
+                        <div className="d-flex align-items-center px-2 justify-content-between">
+                        <div className="pv-more-detail">
+                            <div className="pv-more-title text-capitalize">
+                                buy now
+                            </div>
+                            <div className="pv-more-p">
+                                Pre-loved item excellent condition
+                            </div>
+                            <div className="pv-more-price">
+                                Rs. 3800
+                            </div>
+                        </div>
+                        </div>
+
+                        
+                    </div>
+
+
+                    <div id="pv-more-detail">
+                       <div className="pv-product-detail  d-flex justify-content-between">
+                       <div className="pv-product-detail-head">Product Details</div>
+                        <div className="pv-product-detail-icon" onClick={plusIcon}>
+                            {
+                                state? "-" : "+"
+                            }
+                        </div>
+                       </div>
+
+                        <div className="pv-detail-para">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, sint? Quisquam possimus laudantium iusto repellat praesentium sapiente dolore eos. Est laboriosam nemo recusandae?
+                        </div>
+                    </div>
+
+                    <div id="pv-more-detail">
+                       <div className="pv-product-detail  d-flex justify-content-between">
+                       <div className="pv-product-detail-head">size</div>
+                        <div className="pv-product-detail-icon" onClick={sizeIcon}>
+                            {
+                                size? "-" : "+"
+                            }
+                        </div>
+                       </div>
+
+                        <div className="pv-detail-size">
+                            <ul>
+                                <li> <input type="radio" name="size" /> 80 <i class="fa-solid fa-xmark"></i> 32</li>
+                                <li> <input type="radio" name="size" /> 80 <i class="fa-solid fa-xmark"></i> 26</li>
+                                <li> <input type="radio" name="size" /> 75 <i class="fa-solid fa-xmark"></i>  26</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div id="pv-more-detail">
+                       <div className="pv-product-detail  d-flex justify-content-between">
+                       <div className="pv-product-detail-head">Share</div>
+                        <div className="pv-product-detail-icon" onClick={shareIcon}>
+                            {
+                                state? "-" : "+"
+                            }
+                        </div>
+                       </div>
+
+                        <div className="pv-detail-share">
+                            <div className="pv-fb"><i class="fa-brands fa-facebook"></i></div>
+                            <div className="pv-linked"><i class="fa-brands fa-linkedin"></i></div>
+                            <div className="pv-linked"><i class="fa-brands fa-linkedin"></i></div>
+                            <div className="pv-linked"><i class="fa-brands fa-linkedin"></i></div>
+                            <div className="pv-linked"><i class="fa-brands fa-linkedin"></i></div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
+             <div className="row like-row">
+                <div className="pl d-flex justify-content-between">
+                    <div className="pl-left">You may also Like</div>
+                    <div className="pl-right">View all</div>
+                </div>
+                {/* slick */}
+                <div>
+                  
+                    <Slider {...settings}>
+                     {  likeimg.map((e)=>{
+                        return<div>
+                            <div className="like-card">
+                                <div className="like-img" style={{background: `url(${e.image})center/contain no-repeat`}}>
+                                    <div className="heart" onClick={heart}><i class="fa-solid fa-heart"></i></div>
+                                </div>
+                                <div className="like-detail">
+                                    <div className="like-p-title">Premium Double Doors</div>
+                                    <div className="like-p-model">SD1103</div>
+                                    <div className="like-p-price">Rs. 2800/-</div>
+                                </div>
+                            </div>
+                        </div>
+                        })
+                     }
+                    </Slider>
+                </div>
+            </div>                   
+            
+            <div className="row look-row text-center ">
+                <div className="col-6">
+                <div className="look-head">What are you looking for ?</div>
+                <div className="look-para">We'll help you find a perfect door for your house.</div>
+                <div><div className="look-btn">see more</div></div>
+                </div>
+            </div>
+            <div className="pv-testo">
+            <div id="testomial" class="carousel slide" data-bs-ride="carousel" >
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000" >
+                        <div className="row row-carousel-item">
+                            <div className="col-md-4  col-sm-12 testomial-img"></div>
+                            <div className="col-md-8 col-sm-12 testomial-detail">
+                                <div className="testo-quote-left"><i class="fa-solid fa-quote-left"></i></div>
+                                <div className="testomial-title">
+                                    <div className="testo-name">ram babu gurung</div>
+                                    <div className="testo-position"> - businessman</div>
+                                </div>
+                                <div className="testo-head">amazing customer service</div>
+                                <div className="testo-para">
+                                    
+                                We have purchased this for our office space and are extremely thankful for an entire team for their patience and information provided for our purchase related decisions. Material hardness can be ensured on the spot and every doors are perfectly...
+                                <div className="testo-quote-right"><i class="fa-solid fa-quote-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item"  data-bs-interval="2000" >
+                        <div className="row row-carousel-item">
+                                <div className="col-md-4  col-sm-12 testomial-img"></div>
+                                <div className="col-md-8 col-sm-12 testomial-detail">
+                                    <div className="testo-quote-left"><i class="fa-solid fa-quote-left"></i></div>
+                                    <div className="testomial-title">
+                                        <div className="testo-name">ram babu gurung</div>
+                                        <div className="testo-position"> - businessman</div>
+                                    </div>
+                                    <div className="testo-head">amazing customer service</div>
+                                    <div className="testo-para">
+                                        
+                                    We have purchased this for our office space and are extremely thankful for an entire team for their patience and information provided for our purchase related decisions. Material hardness can be ensured on the spot and every doors are perfectly...
+                                    <div className="testo-quote-right"><i class="fa-solid fa-quote-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div className="row row-carousel-item">
+                                <div className="col-md-4  col-sm-12 testomial-img"></div>
+                                <div className="col-md-8 col-sm-12 testomial-detail">
+                                    <div className="testo-quote-left"><i class="fa-solid fa-quote-left"></i></div>
+                                    <div className="testomial-title">
+                                        <div className="testo-name">ram babu gurung</div>
+                                        <div className="testo-position"> - businessman</div>
+                                    </div>
+                                    <div className="testo-head">amazing customer service</div>
+                                    <div className="testo-para">
+                                        
+                                    We have purchased this for our office space and are extremely thankful for an entire team for their patience and information provided for our purchase related decisions. Material hardness can be ensured on the spot and every doors are perfectly...
+                                    <div className="testo-quote-right"><i class="fa-solid fa-quote-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#testomial" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon fa-solid fa-chevron-left testo-prev" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#testomial" data-bs-slide="next">
+                    <span class="carousel-control-next-icon fa-solid fa-chevron-right testo-prev" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
         </div>
     )
 }
