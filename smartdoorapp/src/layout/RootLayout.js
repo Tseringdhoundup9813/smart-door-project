e4e58a83e35734074072c3e82157d7e4ec3e553d
TseringDhoundup1997 <47768382+Tseringdhoundup9813@users.noreply.github.com>
2023-03-28 17:50:29 +0545

    t product view sloved

--- smartdoorapp/src/layout/RootLayout.js
+++ smartdoorapp/src/layout/RootLayout.js
@@ -1,11 +1,21 @@
 import { Outlet, NavLink } from "react-router-dom";
+import React,{useContext,useState} from "react";
+import Login from "../components/Login";
+import { loginContext } from "../components/Context";
 
 
 
 export default function RootLayout() {
+
+
   return (
+
     <div className="root-layout">
-        <Outlet />
+
+         <Outlet />
+  
+
     </div>
+
   )
 }
